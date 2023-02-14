31d56ced66e4f13b270565b7ea7ff9057070479c
PabloCalabro <pablocala@live.com>
2023-02-14 11:09:49 -0300

    First Commit on development

---
+++ invoice/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("LogaliGroup.invoice.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
